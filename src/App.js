825bdfe6e31d6587eed16a2eb4973b4c0abb0350
Chirag B Shetty <82666275+cs400610@users.noreply.github.com>
2021-11-20 21:08:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios';
+import Navbar from './components/Navbar';
+import Usercard from './components/Usercard';
+import Loader from './components/Loader';
+  
+
 
 function App() {
-  return (
+
+  const [isClicked, setisClicked] = useState(0);
+  const [data, setData] = useState(null);
+  const [isLoader, setisLoader] = useState(false);
+
+  const getUsers = () => {
+
+    setisLoader(true);
+    axios.get("https://reqres.in/api/users?page=1").then((response) =>{
+      console.log("api data recievd");
+      setData(response.data.data);
+      setisLoader(false);
+    });
+  };
+
+  useEffect(() => {
+    isClicked && getUsers();
+  },[isClicked])
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar onSetIsClicked={setisClicked}/>{" "}
+      <div className="row">
+        {" "}
+
+        {data &&
+        data.map((val)=>{
+          return(
+            <Usercard
+            first_name={val.first_name}
+            last_name={val.last_name}
+            email={val.email}
+            avatar={val.avatar}
+            />
+          );
+        })}{" "}
+
+
+      </div>
+      <Loader show={isLoader}/>{" "}
+
+
     </div>
   );
 }
